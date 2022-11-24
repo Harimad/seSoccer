115d706efde01bce92fdb5bc883e4e6aa9387170
Jaebin <wjdwoqls777@naver.com>
2022-11-24 19:30:22 +0900

    jaebin

---
+++ src/components/Jaebin/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleDiv } from "./style.js";
+
+function Jaebin() {
+  return (
+    <div>
+      <StyleDiv>Jaebin</StyleDiv>
+    </div>
+  );
+}
+
+export default Jaebin;
