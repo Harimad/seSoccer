2f64ac0b0a61949c8dfc56eb18d9926658e6cbc6
yongjae95 <86768647+yongjae95@users.noreply.github.com>
2022-11-24 19:36:01 +0900

    Revert "edit file"

--- src/components/YongJae/style.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { FontSize } from "../../constants";
-
-export const StyledDiv = styled.div`
-  font-size: ${FontSize.large};
-  color: yellow;
-  background: purple;
-`;
