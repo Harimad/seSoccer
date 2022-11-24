9fff416e42ed90bc1d96f2cee54c06fd5c8cf5bc
jaeeun <jaeeun9862@gmail.com>
2022-11-24 19:29:17 +0900

    1st commit

---
+++ src/components/jaeeun/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {StyledDiv} from "./style.js";
+
+
+const Jaeeun = () => {
+  return <StyledDiv>Jaeeun</StyledDiv>;
+};
+
+// export default 는 하나의 값만 전달
+export default Jaeeun;
