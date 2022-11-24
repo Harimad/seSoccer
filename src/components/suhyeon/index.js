71fdcf25c56aeb60ce0e4e955aad055c55a16985
soohyun <suhyeon.kan@gmail.com>
2022-11-24 19:29:46 +0900

    edit file

---
+++ src/components/suhyeon/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyledDiv } from './style.js';
+function Suhyeon() {
+	return (
+		<div>
+			<StyledDiv>Suhyeon</StyledDiv>
+		</div>
+	);
+}
+
+export default Suhyeon;
---
+++ src/components/suhyeon/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { FontSize } from '../../constants';
+
+export const StyledDiv = styled.div`
+	font-size: ${FontSize.large};
+	color: yellow;
+	background: purple;
+`;
