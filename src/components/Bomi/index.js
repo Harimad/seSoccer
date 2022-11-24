b5beae8d96719947460e90dfaf44c325036b87e1
sognociel <sognociel@gmail.com>
2022-11-24 19:28:46 +0900

    styled

---
+++ src/components/Bomi/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyledDiv } from "./style.js";
+
+const Bomi = () => {
+  return (
+    <>
+      <StyledDiv>Bomi</StyledDiv>
+      <StyledDiv color={"blue"}>Bomi</StyledDiv>
+    </>
+  );
+};
+
+export default Bomi;
